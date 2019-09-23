3ad5ecce8b495f2548d55bf943ab0686d7e99002
MadHuslista <nicolaschiappacasse@gmail.com>
2019-09-23 14:24:52 -0300

    Idea para generar la interfaz

---
+++ Interfaz.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 23 13:43:59 2019
+
+@author: meyerhof
+"""
+
+"""
+Llevar todo el Model  a una función. 
+Aquí armar luego, varias sliders. 
+Cada slider para cada parámetro 
+    - Aquí armar el circulito de las posiciones. 
+    - Quizás armar una gaussiana genérica para setear amplitud y duración 
+Luego, crear un botón de "GENERAR", 
+    - Aquí en el update llamar a todo el Model con todo lo seteado
+""" 
