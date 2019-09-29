53dc1ea6c4e5178acdc19875173af0a26b4204d7
MadHuslista <nicolaschiappacasse@gmail.com>
2019-09-29 01:55:36 -0300

    Se logró probar la ruta de data frame -> generator -> rand_func -> yield -> arg_update_anim

--- test_animated_line.py
+++ test_animated_line.py
@@ -16,39 +16,30 @@ fig, ax = plt.subplots()
 x = np.arange(0, 2*np.pi, 0.01)
 line, = ax.plot(x, x)
 
-def pp(): 
+gen = 0
+
+def pp(i): 
     print("perro")
+    gen = i
+    return gen , 2
+    #global gen
+    #gen = i
 
 def iterat(n): 
     num = 5
     n = num +n
     while num < n: 
-        pp()
-        yield num 
-        num += 1
-        
+        d = pp(num)
+        print(d)
+        yield d
+        num += 1    
 
-
-def init():  # only required for blitting to give a clean slate.
-    line.set_ydata([np.nan] * len(x))
+def animate(h):
+    line.set_ydata((x + h[0] / 200))  # update the data.
     return line,
 
 
-def animate(i):
-    line.set_ydata((x + i / 200))  # update the data.
-    return line,
+ani = animation.FuncAnimation(fig, animate, frames=iterat(i), interval=2, blit=True, save_count=50)
 
-
-ani = animation.FuncAnimation(fig, animate, init_func=init, interval=2, blit=True, save_count=50)
-
-# To save the animation, use e.g.
-#
-# ani.save("movie.mp4")
-#
-# or
-#
-# from matplotlib.animation import FFMpegWriter
-# writer = FFMpegWriter(fps=15, metadata=dict(artist='Me'), bitrate=1800)
-# ani.save("movie.mp4", writer=writer)
 
 plt.show()
