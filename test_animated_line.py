b2481dd601244552094829039f93082754bfb96d
MadHuslista <nicolaschiappacasse@gmail.com>
2019-09-29 02:35:50 -0300

    Completé la ruta de data. Ahora rand_func es capaz de tomar una variable global que en paralelo puede ser actualizada por update_slider. Por tanto, puedo así modificar las variables de animación.

--- test_animated_line.py
+++ test_animated_line.py
@@ -9,18 +9,37 @@ Created on Wed Aug 21 23:51:49 2019
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+from matplotlib.widgets import Slider, Button, RadioButtons
 plt.close("all")
 
+#Anim
 fig, ax = plt.subplots()
-
 x = np.arange(0, 2*np.pi, 0.01)
 line, = ax.plot(x, x)
 
+#Slider 
+fig_s, ax_s = plt.subplots()
+fig_s.subplots_adjust(left=0.25, bottom=0.25)
+axcolor = 'lightgoldenrodyellow'
+axfreq = fig_s.add_axes([0.25, 0.1, 0.65, 0.03], facecolor=axcolor)
+sfreq = Slider(axfreq, 'Freq', 1, 1000, valinit=0)
+
 gen = 0
+
+def update(val):
+    global gen
+    gen = sfreq.val
+    print(gen)
+    #l.set_ydata(amp*np.sin(2*np.pi*freq*t))
+    #fig.canvas.draw_idle()
+    #print(next(iterat()))
+
+sfreq.on_changed(update)
+
 
 def pp(i): 
     print("perro")
-    gen = i
+    global gen
     return gen , 2
     #global gen
     #gen = i
