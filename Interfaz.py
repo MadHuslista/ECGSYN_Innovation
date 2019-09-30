5dda02dd7bbca60044d3ca5c78c60b1f1a4597c9
MadHuslista <nicolaschiappacasse@gmail.com>
2019-09-30 10:49:37 -0300

    Estética

--- Interfaz.py
+++ Interfaz.py
@@ -6,19 +6,6 @@ Created on Mon Sep 23 13:43:59 2019
 @author: meyerhof
 """
 
-"""
-Llevar todo el Model  a una función. 
-Aquí armar luego, varias sliders. 
-Cada slider para cada parámetro 
-    - Aquí armar el circulito de las posiciones. 
-    - Quizás armar una gaussiana genérica para setear amplitud y duración 
-Luego, crear un botón de "GENERAR", 
-    - Aquí en el update llamar a todo el Model con todo lo seteado
-""" 
-
-
-
-
 import math as m
 import numpy as np
 import matplotlib.pyplot as plt 
